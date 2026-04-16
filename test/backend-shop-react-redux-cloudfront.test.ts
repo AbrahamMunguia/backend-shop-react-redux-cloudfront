6b8f61a23ecfcf7ab53988f05179e63da0ebe0dc
cesar munguia <c.munguiabrito@gmail.com>
2026-04-16 08:51:06 -0600

    feat: added boilerplate

---
+++ test/backend-shop-react-redux-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BackendShopReactReduxCloudfront from '../lib/backend-shop-react-redux-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/backend-shop-react-redux-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BackendShopReactReduxCloudfront.BackendShopReactReduxCloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
