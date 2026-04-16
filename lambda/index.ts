98a1d7c77b500559937bc95bc169cce6be969c40
cesar munguia <c.munguiabrito@gmail.com>
2026-04-16 12:28:46 -0600

    fix: added the product

--- lambda/index.ts
+++ lambda/index.ts
@@ -5,5 +5,15 @@ import { products } from '../lib/faker'
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello Hono!'))
+//Products lambda
 app.get('/products', (c) => c.json(products, 200))
+app.get('/products/:uuid', (c) => {
+    const uuid = c.req.param('uuid')
+    const product = products.find((product) => product.uuid === uuid)
+    if (product) return c.json(product, 200)
+    return c.json({
+        error: `product not found`
+    }, 400)
+}
+)
 export const handler = handle(app)
--- lib/faker.ts
+++ lib/faker.ts
@@ -2,11 +2,11 @@ import { faker } from "@faker-js/faker";
 
 export function createRandomProducts() {
     return {
-        userId: faker.string.uuid(),
+        uuid: faker.string.uuid(),
         product: faker.commerce.product(),
         productDescription: faker.commerce.productDescription(),
         price: faker.commerce.price(),
-        department: faker.commerce.department()
+        department: faker.commerce.department(),
     };
 }
 export const products = faker.helpers.multiple(createRandomProducts, {
